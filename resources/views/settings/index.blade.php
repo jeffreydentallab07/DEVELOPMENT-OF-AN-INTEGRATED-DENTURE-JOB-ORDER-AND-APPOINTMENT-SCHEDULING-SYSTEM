217849a6861b977c69db577e7d3c30edc75bc0dd
jeffreydentallab07 <jeffreydentallab07@gmail.com>
2025-11-24 08:38:17 +0800

    change password noti

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -156,25 +156,6 @@
                             <input type="password" name="new_password_confirmation" required
                                 class="w-full border-2 border-gray-300 rounded-lg p-3 focus:border-blue-500 focus:outline-none">
                         </div>
-
-                        <div class="bg-yellow-50 border-l-4 border-yellow-500 p-4 rounded">
-                            <div class="flex items-start">
-                                <svg class="w-5 h-5 text-yellow-500 mt-0.5 mr-3" fill="currentColor"
-                                    viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                <div>
-                                    <h4 class="text-sm font-semibold text-yellow-800">Security Notice</h4>
-                                    <p class="text-xs text-yellow-700 mt-1">
-                                        Changing your password will log you out. You'll need to sign in again with your
-                                        new password.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="flex justify-end pt-4 border-t">
                             <button type="submit"
                                 class="px-6 py-3 bg-red-600 text-white rounded-lg hover:bg-red-700 transition font-semibold">
