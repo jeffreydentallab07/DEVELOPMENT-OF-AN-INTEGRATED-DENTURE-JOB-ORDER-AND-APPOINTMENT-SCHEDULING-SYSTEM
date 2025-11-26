03e03350a8bcbce9acb3ac232d17327f808a6bc7
jeffreydentallab07 <jeffreydentallab07@gmail.com>
2025-11-26 13:24:44 +0800

    hide search bar in cinic blade

--- resources/views/layouts/clinic.blade.php
+++ resources/views/layouts/clinic.blade.php
@@ -122,13 +122,13 @@
             </button>
 
             <!-- Search Bar (Hidden on small screens) -->
-            <div class="hidden lg:flex items-center gap-2 border border-gray-300 rounded-lg w-96 p-2 bg-white">
+            {{-- <div class="hidden lg:flex items-center gap-2 border border-gray-300 rounded-lg w-96 p-2 bg-white">
                 <svg class="w-4 h-4 text-gray-300 mr-2" fill="currentColor" viewBox="0 0 24 24">
                     <path
                         d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5z" />
                 </svg>
                 <input type="text" placeholder="Search..." class="outline-none w-full text-[12px]">
-            </div>
+            </div> --}}
 
 
             <div class="flex items-center gap-1.5 sm:gap-2">
